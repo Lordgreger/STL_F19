2a536856db8a02f4038c9dfd99f47fdb9866904f
Greger <blacknedpsi@gmail.com>
2019-09-26 16:51:41 +0200

    Some new stuff

---
+++ Assets/Scripts/NewGrid/PlayerGrid.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrid : MonoBehaviour {
+    #region Variables
+    // Public
+    public int gridWidth;
+    public int gridHeight;
+    public int gridValMax;
+    public int gridValMin;
+    public int targetValMax;
+    public int targetValMin;
+
+    // Private
+    Element[,] elementGrid;
+    bool selectorActive;
+    int selectorX;
+    int selectorY;
+
+    // Definitions
+    struct Element {
+        public int value;
+    }
+
+    #endregion
+
+    #region Unity Scheduling
+    private void Start() {
+        elementGrid = new Element[gridWidth, gridHeight];
+        initElementGrid(elementGrid);
+        printElementGrid(elementGrid);
+    }
+
+    private void Update() {
+        
+    }
+    #endregion
+
+    #region Internal Grid
+    void initElementGrid(Element[,] grid) {
+        for (int i = 0; i < grid.GetLength(0); i++) {
+            for (int j = 0; j < grid.GetLength(1); j++) {
+                grid[i, j] = new Element();
+                grid[i, j].value = Random.Range(gridValMin, gridValMax + 1);
+            }
+        }
+    }
+
+    #endregion
+
+    #region Selector
+
+    #endregion
+
+    #region Print
+    void printElementGrid(Element[,] grid) {
+        string output = "";
+        for (int i = grid.GetLength(1) - 1; i >= 0; i--) {
+            for (int j = 0; j < grid.GetLength(0); j++) {
+                output += " " + grid[i, j].value;
+            }
+            output += "\n";
+        }
+
+        Debug.Log(output);
+    }
+    #endregion
+
+
+}
