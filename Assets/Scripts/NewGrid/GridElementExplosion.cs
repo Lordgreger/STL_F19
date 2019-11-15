984b19a58f381077965d3bf1473e1b7de2fdf63c
Greger <blacknedpsi@gmail.com>
2019-11-15 10:31:48 +0100

    explosion!

--- Assets/Scripts/NewGrid/GridElementExplosion.cs
+++ Assets/Scripts/NewGrid/GridElementExplosion.cs
@@ -3,5 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridElementExplosion : MonoBehaviour {
+    public Animator animator;
+    public float explosionTime;
 
+    private void Start() {
+        StartCoroutine(explode(1f));
+    }
+
+    IEnumerator explode(float delay) {
+        yield return new WaitForSeconds(delay);
+        animator.SetTrigger("Explode");
+
+        Destroy(gameObject, explosionTime);
+    }
 }
